3a5a9552fdc070289510b89cbee266cf96fdbf4c
ME <me@example.com>
2019-06-14 05:44:34 +0300

    Testing refactored

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@
 #include "ConvertedLuaPortrayal/ConvertedLua.h"
 #include "GeneratedLists.h"
 
+void doTest(TestObjectDrawer &drawer, std::vector<int> &testFeatures, const int countRuns, bool drawCalls = false);
+
 void printCatalogueStuff(TestObjectDrawer &drawer) {
     for (const auto &x : drawer.GetCatalogue().attributes_complex) {
         std::cout << x.first << " = 'complex',\n";
@@ -78,17 +80,30 @@ int main() {
 //    freopen("out.txt", "w", stdout);
 //    freopen("err.txt", "w", stderr);
 
-    TestObjectDrawer drawer = readFromXML("maps/101CA00270084.usf.xml");
-    std::vector<int> excluded = {11735, 11746, 11724, 11744, 11745, 12691, 12694, 12695, 12720, 12721, 12669, 12670,
-                                 12671, 12672, 12673, 12674, 12675, 12676, 11748};
-    for (auto x : excluded)
-        drawer.RemoveFeature(x);
-//    TestObjectDrawer drawer = readFromAllXML();
-//    return 0;
-    std::vector<int> testFeatures;// = getGeneratedLists().featuresList;
-
+    std::string m = "maps/";
+    std::vector<std::string> maps = {"101CA00173289", "101CA00273313", "101CA00376166", "101CA00470351",
+                                     "101CA00573356", "101CA00673225"};
+    std::vector<int> testFeatures;
 //    const int countRuns = 1;
     const int countRuns = 500;
+    for (auto &x : maps) {
+        std::cout << x << "\n";
+        TestObjectDrawer drawer = readFromXML(m + x + ".usf.xml");
+        doTest(drawer, testFeatures, countRuns);
+    }
+
+
+//    std::vector<int> excluded = {11735, 11746, 11724, 11744, 11745, 12691, 12694, 12695, 12720, 12721, 12669, 12670,
+//                                 12671, 12672, 12673, 12674, 12675, 12676, 11748};
+//    for (auto x : excluded)
+//        drawer.RemoveFeature(x);
+//    TestObjectDrawer drawer = readFromAllXML();
+//    return 0;
+
+    return 0;
+}
+
+void doTest(TestObjectDrawer &drawer, std::vector<int> &testFeatures, const int countRuns, bool drawCalls) {
     const int warmupRuns = std::min(500, countRuns / 5);
 
 //    printCatalogueStuff(drawer);
@@ -114,28 +129,35 @@ int main() {
         if (i >= warmupRuns)
             cpp += rs;
     }
-//    th[0] = runInitialLuaCold(drawer, "out\\thlua\\", countRuns, testFeatures);
-//    th[1] = runInitialLuaShort(drawer, "out\\thlua\\", countRuns, testFeatures);
-//    th[2] = runInitialLuaLong(drawer, "out\\thlua\\", countRuns, testFeatures);
-//    std::cerr.flush();
-//    std::cout << drawer.drawCalls / (countRuns) << " draw calls\n";
-//    std::cerr.flush();
-//    th[3] = runInitialLuaCreate(drawer, "out\\thlua\\", countRuns, testFeatures);
+    th[0] = runInitialLuaCold(drawer, "out\\thlua\\", countRuns, testFeatures);
+    if (drawCalls) {
+        std::cerr.flush();
+        std::cout << drawer.drawCalls / (countRuns) << " draw calls\n";
+        std::cerr.flush();
+    }
 
     opt[0] = runModifiedLuaCold(drawer, "out\\modlua\\", countRuns, testFeatures);
-    opt[1] = runModifiedLuaShort(drawer, "out\\modlua\\", countRuns, testFeatures);
-    opt[2] = runModifiedLuaLong(drawer, "out\\modlua\\", countRuns, testFeatures);
-    std::cerr.flush();
-    std::cout << drawer.drawCalls / (countRuns) << " draw calls\n";
-    std::cerr.flush();
-    opt[3] = runModifiedLuaCreate(drawer, "out\\modlua\\", countRuns, testFeatures);
+    if (drawCalls) {
+        std::cerr.flush();
+        std::cout << drawer.drawCalls / (countRuns) << " draw calls\n";
+        std::cerr.flush();
+    }
 //
     conv[0] = runMyLuaCold(drawer, "out\\modlua\\", countRuns, testFeatures);
+    if (drawCalls) {
+        std::cerr.flush();
+        std::cout << drawer.drawCalls / (countRuns) << " draw calls\n";
+        std::cerr.flush();
+    }
+
+    th[1] = runInitialLuaShort(drawer, "out\\thlua\\", countRuns, testFeatures);
+    opt[1] = runModifiedLuaShort(drawer, "out\\modlua\\", countRuns, testFeatures);
     conv[1] = runMyLuaShort(drawer, "out\\modlua\\", countRuns, testFeatures);
+    th[2] = runInitialLuaLong(drawer, "out\\thlua\\", countRuns, testFeatures);
+    opt[2] = runModifiedLuaLong(drawer, "out\\modlua\\", countRuns, testFeatures);
     conv[2] = runMyLuaLong(drawer, "out\\modlua\\", countRuns, testFeatures);
-    std::cerr.flush();
-    std::cout << drawer.drawCalls / (countRuns) << " draw calls\n";
-    std::cerr.flush();
+    th[3] = runInitialLuaCreate(drawer, "out\\thlua\\", countRuns, testFeatures);
+    opt[3] = runModifiedLuaCreate(drawer, "out\\modlua\\", countRuns, testFeatures);
     conv[3] = runMyLuaCreate(drawer, "out\\modlua\\", countRuns, testFeatures);
 
     for (int i = 0; i < 4; i++) {
@@ -148,5 +170,4 @@ int main() {
 //        std::cout << "Converted Lua: " << conv / countRuns << " usec\n";
 //        std::cout << "Converted C++: " << cpp / countRuns << " usec\n";
     }
-    return 0;
 }
